a13696164edc8d9a26406c9f19a86c8378520a65
Harman Singh <harman.j.singh@hotmail.com>
2017-05-07 15:21:01 -0400

    Added insert sort

--- OCAMLElementary.ml
+++ OCAMLElementary.ml
@@ -7,6 +7,8 @@ in
     | [] -> 0
     | _ :: rest -> 1 + mylength rest
 
+(*///////////////////////////////////////////////////////////////*)
+
 let square x = x * x ;;
 
 square 3 ;;
@@ -20,4 +22,46 @@ let squareDebug = square 120;;
 
 let () = Printf.printf "Fact: %d\n" factDebug
 let () = Printf.printf "SquareDebug: %d\n" squareDebug   
+
+(*
+Automatic memory management
+
+All allocation and deallocation operations are fully automatic. 
+For example, let us consider simply linked lists.
+
+Lists are predefined in OCaml. The empty list is written []. 
+The constructor that allows prepending an element to a list is 
+written :: (in infix form).
+
+*)
+
+let li = 1 :: 2 :: 3 :: [] ;; 
+(* val li : int list = [1; 2; 3] *)
+
+let a = [1; 2; 3; 4; 5];;
+
+[1; 2; 3] ;;
+(* - : int list = [1; 2; 3] *)
+
+5 ::  li ;;
+(* - : int list = [5; 1; 2; 3] *)
+  
+(*
+Polymorphism: 
+Insertion sort is defined using two recursive functions.
+
+Note that the type of the list elements remains unspecified: 
+it is represented by a type variable 'a. Thus, sort can be 
+applied both to a list of integers and to a list of strings.
+
+*)
+
+let rec sort = function 
+    | [] -> []
+    | x :: l -> insert x (sort l)
+and insert elem = function
+    | [] -> [elem]
+    | x :: l -> if elem < x then elem :: x :: l
+                else x :: insert elem l ;;
+
 
