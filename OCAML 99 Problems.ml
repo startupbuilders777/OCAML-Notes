04dec40eaef968f428353948c75ecb3ec4767413
Harman Singh <harman.j.singh@hotmail.com>
2017-05-08 16:44:08 -0400

    #Add OCAML Language Notes 2 for and keyword and local and global variables

--- OCAML 99 Problems.ml
+++ OCAML 99 Problems.ml
@@ -10,3 +10,74 @@ let rec last lst =
 
 last ["a" , "b", "c", "d"];; (* DO NOT DO THIS LIST IS SEPERATED BY SEMICOLONS*)
 last ["a"; "b"; "c" ; "d"];; 
+
+(*
+Find the last but one (last and penultimate) elements of a list. (easy)
+# last_two [ "a" ; "b" ; "c" ; "d" ];;
+- : (string * string) option = Some ("c", "d")
+# last_two [ "a" ];;
+- : (string * string) option = None
+*)
+
+let rec last_two lst = 
+    match lst with 
+    | [] -> None
+    | [a] -> None
+    | [ a; b ] -> Some [a;b]
+    | hd :: tl -> last_two tl;;
+last_two [ "a" ; "b" ; "c" ; "d" ];;
+last_two [ "a" ];;
+
+(*
+Find the k'th element of a list. (easy)
+# # at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
+- : string option = Some "c"
+# at 3 [ "a" ];;
+- : string option = None
+
+- : string option = Some "c"
+# at 3 [ "a" ];;
+- : string option = None
+*)
+
+let rec at k = function
+    | [] -> None
+    | hd::tl -> if k = 1 then Some hd else at (k-1) tl;;
+    
+
+let x = at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
+
+(*
+
+13
+down vote
+accepted
+The traditional way to obtain the value inside any kind of constructor in OCaml is with pattern-matching. 
+Pattern-matching is the part of OCaml that may be most different from what you have already seen in 
+other languages, so I would recommend that you do not just write programs the way you are used to 
+(for instance circumventing the problem with ocaml-lib) but instead try it and see if you like it.
+*)
+
+match x with 
+| None ->  Printf.printf "saw nothing at all\n"
+| Some a -> Printf.printf "Fact: %s\n" a;;
+
+(*
+Find the number of elements in a list
+# length [ "a" ; "b" ; "c"];;
+- : int = 3
+# length [];;
+- : int = 0
+*)
+
+let length lst = 
+    let rec length_rec counter = function
+    | [] -> counter
+    | hd::tl -> length_rec (counter+1) tl in
+    length_rec 0 lst ;;
+
+(*
+
+*)
+
+let reverse lst = 
