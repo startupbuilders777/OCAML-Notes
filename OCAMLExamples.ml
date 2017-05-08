609d2dd87862738054ae7986770e41e5b7421906
Harman Singh <harman.j.singh@hotmail.com>
2017-05-07 20:33:40 -0400

    More OCAML Notes on Higher Order Functions

--- OCAMLExamples.ml
+++ OCAMLExamples.ml
@@ -5,17 +5,38 @@ pair with the input value as both first and second elements.
 *)
 
 let pairify a = (a,a) ;;
-
-print(pairify 2) ;; 
+pairify 2 ;; 
 
 
 (*
 Exercise 2. Write a function curry of type ((’ a ∗ ’b) → ’c) → ’a → ’b → ’c, that, given
 a function that uses a pair to encode parameters, returns a currified function.
+*)
 
+
+let sum (a,b) = a + b ;;
+sum (1,2) ;; 
+
+let currify pairEncodedFunction = fun a b -> pairEncodedFunction(a,b) ;;
+
+let currifiedSum = currify sum ;;
+
+let currifiedSumWith4 = currifiedSum 4 ;;
+
+let () = Printf.printf "currifier: %d\n" (currifiedSumWith4 6);; 
+
+
+(*
 Exercise 3. Write a function decurry of type (’ a → ’b → ’c) → (’a ∗ ’b) → ’c that performs
 the opposite operation.
+*)
 
+let decurry curriedFunction = 
+    let pairedFunction (a, b) = curriedFunction a b
+    pairedFunction;;
+
+
+(*
 Exercise 4. The function List .map of type (’ a → ’b) → ’a list → ’b list takes a function
 and a list and returns the list obtained by applying the function to each element of the
 initial list. Write a function add1 that takes a list and adds 1 to each element of this list.
