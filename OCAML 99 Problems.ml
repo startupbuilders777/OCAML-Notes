88db0365665fc60674225172f6bab7ebd3341f0f
Harman Singh <harman.j.singh@hotmail.com>
2017-05-09 20:51:42 -0400

    #MORE PROBLEMS

--- OCAML 99 Problems.ml
+++ OCAML 99 Problems.ml
@@ -125,12 +125,106 @@ type 'a node =
     | One of 'a
     | Many of 'a node list;;
 
-let flatten nodes = 
-    match nodes with
+
+let rec append lst1 lst2 = 
+    match (lst1, lst2) with
+    | (h1 :: t1, lst2) -> h1 :: append t1 lst2
+    | ([], lst2) -> lst2
+
+let rec flatten lst = 
+    match lst with
     | [] -> []
-    | h :: t -> 
-    
-    | One of b -> b 
-    | Many of 
+    | One a :: t -> a :: flatten t
+    | Many a :: t -> append (flatten a) (flatten t) ;;
 
+(* Solution 2*)
+let flatten list =
+    let rec aux acc = function
+      | [] -> acc
+      | One x :: t -> aux (x :: acc) t
+      | Many l :: t -> aux (aux acc l) t in
+    List.rev (aux [] list);;
 
+(* Eliminate consecutive duplicates of list elements. (medium)
+# compress ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+- : string list = ["a"; "b"; "c"; "a"; "d"; "e"]
+ *)
+
+(*THis removes all duplicates not even consecutive*)
+let rec isItInList elem = function
+    | [] -> false
+    | h :: t when h = elem -> true
+    | _ :: t -> isItInList elem t  ;;
+
+let compress lst = 
+    let rec aux acc = function
+        | [] -> acc
+        | h :: t when not (isItInList h acc) -> aux (h::acc) t
+        | _ :: t -> aux acc t  in
+    List.rev (aux [] lst)
+
+(* This removes consecutive  *)
+
+let rec compress = function
+    | a :: (b :: _ as t) -> if a = b then compress t else a :: compress t
+    | smaller -> smaller;;
+
+(*Pack consecutive duplicates of list elements into sublists. (medium) *)
+
+(*
+//////////////////////////////////////////////////////////////////////////////
+//////////////PLEASE COMPLETE THIS/////////////////////////////////////////
+(*///////////////////////////////////////////////////////////////////////////*)
+let rec pass lst =
+    let rec pass_rec acc lst = 
+    match (acc, lst) with
+        | ([], []) -> []
+        | ([], lsth :: lstt) ->  pass_rec(lsth::acc)(lstt)
+        | (acch :: _, lsth::lstt) when acch = lsth -> pass_rec(lsth::acc)(lstt)
+        | (acch :: acct, lsth::lstt) when acch <> lsth -> acc::pass_rec([lsth])(lstt)  in
+    pass_rec [] lst
+*)
+
+(*Run length Encoding Algorithm*)
+
+let encode lst = 
+    let rec encode_rec acc currLetter currCounter = function
+    | [] when currLetter <> "" -> (currCounter, currLetter) :: acc
+    | [] -> acc
+    | h :: tl when h = currLetter -> encode_rec(acc)(currLetter)(currCounter + 1)(tl)
+    | h :: tl when h <> currLetter -> if currLetter = "" then encode_rec acc h 1 tl 
+                                      else encode_rec ((currCounter , currLetter)::acc) h 1 tl in
+    List.rev (encode_rec [] "" 0 lst)
+
+(*
+Modified run-length encoding. (easy)
+
+Modify the result of the previous problem in such a way that if an 
+element has no duplicates it is simply copied into the result list. 
+Only elements with duplicates are transferred as (N E) lists.
+
+Since OCaml lists are homogeneous, one needs to define a type to hold 
+both single elements and sub-lists.
+
+# encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+- : string rle list =
+[Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+ Many (4, "e")]
+*)
+
+ type 'a rle =
+    | One of 'a
+    | Many of int * 'a;;
+(*type 'a rle = One of 'a | Many of int * 'a*)
+
+let encodeX lst = 
+    let rec encode_recX acc currLetter currCounter = function
+        | [] when currLetter = "" -> acc
+        | [] -> if currCounter = 1 then (One currLetter)::acc
+                else Many(currCounter, currLetter)::acc 
+        | h :: t when h = currLetter -> encode_recX(acc)(currLetter)(currCounter + 1)(t)
+        | h :: t when h <> currLetter -> if currLetter = "" then encode_recX acc h 1 t
+                                         else if currCounter = 1 then encode_recX((One currLetter)::acc)(h)(1)(t)
+                                         else encode_recX((Many(currCounter, currLetter))::acc)(h)(1)(t) in
+    List.rev(encode_recX [] "" 0 lst) ;;
+ 
